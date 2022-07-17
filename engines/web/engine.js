0161a336bcbff6e297c41f9a153caabcceae9243
Aid Vllasaliu <aid@superkraft.io>
2022-07-17 09:17:50 +0200

    0.13

--- engines/web/engine.js
+++ engines/web/engine.js
@@ -76,7 +76,7 @@ module.exports = class SK_WebEngine extends SK_RootEngine {
 
         
 
-        app.use('/sk', this.express.static(this.paths.frontend.ss))
+        app.use('/sk', this.express.static(this.paths.frontend.sk))
         
         app.use('/', this.express.static(this.paths.frontend.app))
 
