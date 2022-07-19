61b19f91c36f3cee667b9b7ad131098f0114dbcb
Aid Vllasaliu <aid@superkraft.io>
2022-07-19 11:47:12 +0200

    0.23

--- sk_create_app.js
+++ sk_create_app.js
@@ -1,3 +1,6 @@
+//WIP
+
+
 var fs = require('fs')
 
 //project name
@@ -14,14 +17,30 @@ var terminal = new (require('./modules/sk_terminal.js'))()
 
 
 var start = async ()=>{
+    var sk_root = __dirname.split('\\').join('/')
+    var sk_rootParentPath = sk_root.split('/')
+    sk_rootParentPath.splice(sk_rootParentPath.length - 1, 1)
+    sk_rootParentPath = sk_rootParentPath.join('/') + '/'
+
     var opt = {
-        projectRoot: __dirname
+        paths: {
+            emptySample: __dirname + '/sk_empty_sample/',
+            projectRoot: sk_rootParentPath
+        }
     }
 
     console.log('Target project root: ' + opt.projectRoot)
     
-    opt.projectName = await terminal.query('Project name: '),
+    opt.projectName = await terminal.query('Project name (Use capital letters for acronym. E.g: MySuperKraftApp = MSKA): ')
+
+    opt.projectAcronym = opt.projectName.match(/\p{Lu}/gu).join('').toLowerCase()
+    console.log('Project acronym: ' + opt.projectAcronym)
+
     opt.appType = await terminal.query('App type (web || dapp): ')
+
+    console.log('Creating app...')
+
+    fs.cp(opt.paths.emptySample, opt.paths.projectRoot + opt.projectAcronym + '_' + 'root')
 }
     
 
