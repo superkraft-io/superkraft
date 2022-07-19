6e13269ec49a146bc87229d97f03da72c425b053
Aid Vllasaliu <aid@superkraft.io>
2022-07-19 11:56:27 +0200

    0.24

---
+++ sk_empty_sample/app.js
@@ -0,0 +1,15 @@
+var sk = new (require('./superkraft/sk_superkraft.js'))({
+    type: 'web',
+    root: __dirname,
+    projectRoot: __dirname + '/sk_projectRoot',
+    postsRoot: __dirname + '/sk_posts/',
+    templates: __dirname + '/sk_templates/',
+
+    config: '/config.json',
+
+    database : new (require('./modules/sk_db.js'))(),
+    auth     : new (require('./modules/sk_auth.js'))(),
+    l10n     : new (require('./modules/sk_l10n.js'))(),
+
+    //useComplexity: true
+})
