06ab218d4970f50fa439e67dca6d808469c88076
Aid <vllasaliu.aid@gmail.com>
2022-09-05 09:35:38 +0200

    0.85

--- ui/frontend/core/sk_ui_checkbox/sk_ui_checkbox.js
+++ ui/frontend/core/sk_ui_checkbox/sk_ui_checkbox.js
@@ -8,21 +8,23 @@ class sk_ui_checkbox extends sk_ui_button {
 
         this.icon = 'check'
 
-        this._checked = false
         this.onClick = ()=>{
             this.checked = !this.checked
         }
 
 
-        this.attributes.add({friendlyName: 'Checked', name: 'checked', type: 'bool', onSet: val => {
-            if (val){
-                this._icon.iconElement.classList.add('sk_ui_checkbox_on')
-                if (this.onChecked) this.onChecked(this)
-            } else {
-                this._icon.iconElement.classList.remove('sk_ui_checkbox_on')
-                if (this.onUnchecked) this.onUnchecked(this)
-            }
 
+        this.attributes.add({friendlyName: 'Checked State', name: 'checked_state', type: 'bool', onSet: val => {
+            if (val) this._icon.iconElement.classList.add('sk_ui_checkbox_on')
+            else this._icon.iconElement.classList.remove('sk_ui_checkbox_on')
+            this.__checked = val
+        }})
+
+        this.attributes.add({friendlyName: 'Checked', name: 'checked', type: 'bool', onSet: val => {
+            this.checked_state = val
+            if (val){ if (this.onChecked) this.onChecked(this) }
+            else { if (this.onUnchecked) this.onUnchecked(this) }
+            
             if (this.onChanged) this.onChanged(val)
         }})
         
