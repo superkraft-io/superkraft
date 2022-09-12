3178c50ce829b487b87a702302f08fea972e34ed
Aid <vllasaliu.aid@gmail.com>
2022-09-12 11:15:39 +0200

    1.8

--- ui/frontend/core/sk_ui_colorPicker/sk_ui_colorPicker.js
+++ ui/frontend/core/sk_ui_colorPicker/sk_ui_colorPicker.js
@@ -3,7 +3,7 @@ class sk_ui_colorPicker extends sk_ui_component {
         super(opt)
 
         this.inputBucket = JSOM.parse({root: this.element, tree: {
-            input_clrPicker: { type: 'color', style: 'width: 100%; height: 100%; border-style: none; padding: 0px; border-width: 0px; background-color: transparent; border-color: transparent;',
+            input_clrPicker: { type: 'color', style: 'opacity: 0; width: 100%; height: 100%; border-style: none; padding: 0px; border-width: 0px; background-color: transparent; border-color: transparent;',
                 events: {
                     input: _e => {
                         this.color = this.inputBucket.clrPicker.value
@@ -14,9 +14,16 @@ class sk_ui_colorPicker extends sk_ui_component {
         }})
 
 
+        this.backgroundColor = '#000000ff'
+        
+
+        this.element.addEventListener('click', ()=>{
+            this.inputBucket.clrPicker.click()
+        })
 
         this.attributes.add({friendlyName: 'Color', name: 'color', type: 'color', onSet: val =>{
             this.inputBucket.clrPicker.value = val
+            this.backgroundColor = val
         }})
     }
 }
