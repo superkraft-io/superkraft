10bbdcd61e2cb996137beb8fc22f7a5e27fc6bab
Aid Vllasaliu <vllasaliu.aid@gmail.com>
2024-07-24 18:54:48 +0200

    3.19

--- engines/japp/virtual_backend/c_backend/sk_vb_c_api/nodejs/electronjs/sk_nodejs_electronjs.cpp
+++ engines/japp/virtual_backend/c_backend/sk_vb_c_api/nodejs/electronjs/sk_nodejs_electronjs.cpp
@@ -11,5 +11,10 @@ SK_VB_ElectronJS::SK_VB_ElectronJS(SK_VirtualBackend* _vbe) {
 
 void SK_VB_ElectronJS::handle_IPC_Msg(String msgID, DynamicObject *obj, String& responseData) {
     var info = obj->getProperty("data");
-    
+
+    String func = info.getProperty("func", "");
+
+    auto x = 0;
 };
+
+//implement the different electronjs functions
