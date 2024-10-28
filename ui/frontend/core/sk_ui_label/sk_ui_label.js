7778cae15437147fdea1aaeb43e1de922a46e080
Aid <vllasaliu.aid@gmail.com>
2024-10-28 19:09:31 +0100

    6.62-m

--- ui/frontend/core/sk_ui_label/sk_ui_label.js
+++ ui/frontend/core/sk_ui_label/sk_ui_label.js
@@ -11,12 +11,16 @@ class sk_ui_label extends sk_ui_component {
         this.attributes.add({friendlyName: 'Text', name: 'text', type: 'text', onSet: async val => {
             if (!this.fadeOnChange){
                 this.__l10n = undefined
-                return this.element.innerHTML = val
+                return this.element.innerHTML = val.split('\n').map(line =>{
+                    return '<div>' + line + '</div>'
+                }).join('')
             }
 
             this.hideShow_2({onHidden: async ()=>{ return new Promise(resolve => {
                 this.__l10n = undefined
-                this.element.innerHTML = val
+                this.element.innerHTML = val.split('\n').map(line =>{
+                    return '<div>' + line + '</div>'
+                }).join('')
                 resolve()
             })}})
         }})
