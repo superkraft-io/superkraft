31c12d6dd63968876c996ac90f819f039e46b919
Aid Vllasaliu <aid@superkraft.io>
2025-05-25 00:16:01 +0200

    Removed JUCE and SSC engines
    They are no longer needed, and we can use the SKXX engine for any C++ framework now.

--- engines/japp/global_actions/relaunch.js
+++
@@ -1,7 +0,0 @@
-module.exports = class SK_Action extends SK_RootAction {
-    exec(opt, res, window){
-        this.sk.app.relaunch()
-        this.sk.engine.terminate()
-        res.resolve({})
-    }
-}
