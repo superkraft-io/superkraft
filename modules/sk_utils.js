b112a1b173a4dde95e00638931136684647b901f
Aid Vllasaliu <vllasaliu.aid@xn--gmil-6na.com>
2025-12-29 19:45:55 +0100

    Updated

--- modules/sk_utils.js
+++ modules/sk_utils.js
@@ -70,10 +70,11 @@ module.exports = class SK_Utils {
 
     checkInternetDNS(){
         return new Promise((resolve) => {
+            if (this.sk.forceOnlineState) return resolve(true)
+                
             const dns = require('dns');
             
             dns.lookup('google.com', (err) => {
-
                 if (err && err.code === 'ENOTFOUND') {
                     resolve(false);
                 } else {
